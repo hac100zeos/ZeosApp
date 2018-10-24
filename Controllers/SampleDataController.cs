c093f52fe839a9d07e22f2c17fdb0bbf3bf7485d
Elliot Lewis <elliotleelewis@gmail.com>
2018-10-24 15:40:26 +0100

    Initial project cleanup

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -1,44 +1,53 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-
+// <copyright file="SampleDataController.cs" company="ZEOS">
+// Copyright (c) ZEOS. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
 namespace ZeosApp.Controllers
 {
-    [Route("api/[controller]")]
-    public class SampleDataController : Controller
-    {
-        private static string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+	using System;
+	using System.Collections.Generic;
+	using System.Globalization;
+	using System.Linq;
+	using Microsoft.AspNetCore.Mvc;
 
-        [HttpGet("[action]")]
-        public IEnumerable<WeatherForecast> WeatherForecasts()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                DateFormatted = DateTime.Now.AddDays(index).ToString("d"),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            });
-        }
+	[Route("api/[controller]")]
+	public class SampleDataController : Controller
+	{
+		private static readonly string[] Summaries =
+		{
+			"Freezing",
+			"Bracing",
+			"Chilly",
+			"Cool",
+			"Mild",
+			"Warm",
+			"Balmy",
+			"Hot",
+			"Sweltering",
+			"Scorching",
+		};
 
-        public class WeatherForecast
-        {
-            public string DateFormatted { get; set; }
-            public int TemperatureC { get; set; }
-            public string Summary { get; set; }
+		[HttpGet("[action]")]
+		public IEnumerable<WeatherForecast> WeatherForecasts()
+		{
+			var rng = new Random();
+			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+			{
+				DateFormatted = DateTime.Now.AddDays(index).ToString("d", new CultureInfo("en")),
+				TemperatureC = rng.Next(-20, 55),
+				Summary = Summaries[rng.Next(Summaries.Length)],
+			});
+		}
 
-            public int TemperatureF
-            {
-                get
-                {
-                    return 32 + (int)(TemperatureC / 0.5556);
-                }
-            }
-        }
-    }
+		public class WeatherForecast
+		{
+			public string DateFormatted { get; set; }
+
+			public int TemperatureC { get; set; }
+
+			public string Summary { get; set; }
+
+			public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+		}
+	}
 }
