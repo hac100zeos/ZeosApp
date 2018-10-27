52d8a689d66e67d76b4a53e0248e646ea9ad078d
Zamir Meah <zamir-meah@hotmail.com>
2018-10-27 17:07:11 +0100

    Added models for product

---
+++ Models/Product.cs
@@ -0,0 +1,29 @@
+using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ZeosApp.Models
+{
+	/// <summary>
+	/// Product model for mongo. Use this model when inserting a new product into the database.
+	/// </summary>
+
+	[BsonIgnoreExtraElements]
+	public class Product
+	{
+		[BsonIgnoreIfNull, BsonIgnore]
+		public ObjectId? Id { get; set; }
+
+		[BsonElement("ProductName")]
+		public string Name { get; set; }
+
+		[BsonElement("Description")]
+		public string Description { get; set; }
+
+		[BsonElement("Category")]
+		public string Category { get; set; }
+		
+		[BsonElement("ProductPicUrl")]
+		public string ImageUrl { get; set; }
+	}
+}
