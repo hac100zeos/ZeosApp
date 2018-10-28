9373da7690a9fce16cf08df60359b8bfdb912a17
Ousmane Diaw <ousmane.diaw@stu.mmu.ac.uk>
2018-10-28 11:20:45 +0000

    half way through basket screen

--- ClientApp/src/app/pages/basket/basket.component.ts
+++ ClientApp/src/app/pages/basket/basket.component.ts
@@ -2,18 +2,23 @@ import { Component, OnInit } from '@angular/core';
 
 import { Product } from '../../models/api/product';
 import { PurchaseService } from '../../services/purchase.service';
-
+import { faMoneyBill } from '@fortawesome/free-solid-svg-icons';
 @Component({
 	selector: 'zeos-basket',
 	templateUrl: './basket.component.html',
 	styleUrls: ['./basket.component.scss'],
 })
 export class BasketComponent implements OnInit {
-	basket: Product[];
+	products: Product[];
+	faMoneyBill = faMoneyBill;
+	totalPrice: number;
 
 	constructor(private _purchase: PurchaseService) {}
 
 	ngOnInit() {
-		this._purchase.basket.subscribe((basket) => (this.basket = basket));
+		this._purchase.basket.subscribe((products) => {
+			this.products = products;
+			this.totalPrice = products.reduce((p1, p2) => p1 + p2.Price, 0);
+		});
 	}
 }
