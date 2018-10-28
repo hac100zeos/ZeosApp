94f5b2df9dbc361ef8291c52e182459ee919d8a0
Zamir Meah <zamir-meah@hotmail.com>
2018-10-28 03:10:56 +0000

    Added dependency injection

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,8 +1,15 @@
 namespace ZeosApp.Controllers
 {
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
 	using Microsoft.AspNetCore.Authorization;
 	using Microsoft.AspNetCore.Mvc;
+	using MongoDB.Bson;
+	using MongoDB.Bson.Serialization;
 	using MongoDB.Driver;
+	using Newtonsoft.Json;
+	using Newtonsoft.Json.Bson;
 	using ZeosApp.Models;
 
 	[Authorize]
@@ -12,16 +19,17 @@ namespace ZeosApp.Controllers
 		private readonly IMongoDatabase database = null;
 		private readonly MongoClient client = null;
 
-		public ProductController()
+		public ProductController(DataAccess access)
 		{
-			client = DataAccess.InitializeClient();
-			database = client.GetDatabase("ZeosAppDB", null);
+			client = access.InitializeClient();
+			database = client.GetDatabase("ZeosApp", null);
 		}
+
 
 		[HttpGet("[action]")]
 		public ActionResult<Product> GetProducts()
 		{
-			var products = database.GetCollection<Product>("product");
+			var products = database.GetCollection<Product>("Products");
 			if (products == null)
 			{
 				return NotFound();
@@ -32,7 +40,7 @@ namespace ZeosApp.Controllers
 		[HttpGet("[action]")]
 		public ActionResult<Product> GetProduct(string name)
 		{
-			var product = database.GetCollection<Product>("product");
+			var product = database.GetCollection<Product>("Products");
 			var result = product.Find(p => p.Name.Equals(name)).Single();
 			if (result == null)
 			{
@@ -45,7 +53,7 @@ namespace ZeosApp.Controllers
 		[HttpGet("[action]")]
 		public ActionResult<Product> SearchProduct(string name)
 		{
-			var products = database.GetCollection<Product>("product");
+			var products = database.GetCollection<Product>("Products");
 			var result = products.Find(p => p.Name.Equals(name)).ToList();
 			if (result == null)
 			{
